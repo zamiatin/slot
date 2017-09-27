4cfb99526644ada6e07dbdf7ec1739d77d80b7f7
zyama <zyama@syneforge.com>
2017-09-27 18:15:37 +0300

    Add styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('gkk');
+var Pixi = require('pixi.js');
+var styles = require('./styles.scss');
+
+var renderer = Pixi.autoDetectRenderer(100, 100);
+document.body.appendChild(renderer.view);
+var stage = new PIXI.Container();
+renderer.render(stage);
+
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+renderer.resize(window.innerWidth, window.innerHeight);
